6c4abbfc5e744e5ef8702f21205343cb4e7f7bf1
AiKaoWa <konowry@gmail.com>
2025-11-23 20:05:58 +0700

    Update logging.h

--- logging.h
+++ logging.h
@@ -1,40 +1,14 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 
-#include <stdio.h>
-#include <time.h>
-#include <string.h>
+#include <time.h> // Necessary for the time-related structures in the implementation
 
-// -----------------------------------------------------------------------------
-// CONTROL FLAG: Define this to enable all logs. Comment it out to disable them.
-// -----------------------------------------------------------------------------
-#define ENABLE_LOGGING
+// Define the name of the file where scores will be appended
+#define LOG_FILENAME "quiz_scores.log"
+#define MAX_LOG_ENTRY_LEN 256
 
-// Extract filename only (remove full path)
-#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-
-// --- Logging Macros ---
-#ifdef ENABLE_LOGGING
-
-#define LOG_INFO(fmt, ...) \
-    fprintf(stdout, "[INFO]  [%s] %s:%d: " fmt "\n", \
-            get_timestamp(), __FILENAME__, __LINE__, ##__VA_ARGS__)
-
-#define LOG_ERROR(fmt, ...) \
-    fprintf(stderr, "[ERROR] [%s] %s:%d: " fmt "\n", \
-            get_timestamp(), __FILENAME__, __LINE__, ##__VA_ARGS__)
-
-#define LOG_DEBUG(fmt, ...) \
-    fprintf(stdout, "[DEBUG] [%s] %s:%d: " fmt "\n", \
-            get_timestamp(), __FILENAME__, __LINE__, ##__VA_ARGS__)
-
-#else
-#define LOG_INFO(fmt, ...)  (void)0
-#define LOG_ERROR(fmt, ...) (void)0
-#define LOG_DEBUG(fmt, ...) (void)0
-#endif
-
-// Correct prototype
-const char* get_timestamp();
+// Function prototype for the logging function defined in logging.c
+// This is the function run_quiz will call.
+void log_score(int score, int total_questions);
 
 #endif // LOGGING_H
