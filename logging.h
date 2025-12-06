3a24a7e6dde0a5e801e7d537de1914ba4269eaa0
senni-huemwang <shuemwang.han@gmail.com>
2025-12-06 12:16:42 +0700

    Update logging.h

--- logging.h
+++ logging.h
@@ -1,14 +1,11 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 
-#include <time.h> // Necessary for the time-related structures in the implementation
+#include <time.h>
 
-// Define the name of the file where scores will be appended
 #define LOG_FILENAME "quiz_scores.log"
 #define MAX_LOG_ENTRY_LEN 256
 
-// Function prototype for the logging function defined in logging.c
-// This is the function run_quiz will call.
 void log_score(int score, int total_questions);
 
 #endif // LOGGING_H
